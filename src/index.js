4701079653086305b02dcfb8774ed82f5017d018
tetuyakouno <65160082+tetuyakouno@users.noreply.github.com>
2021-04-18 10:11:21 +0900

    write index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import "./styles.css";
+const onClickAdd = () => {
+  //  テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+  //  div の生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  //  p タグの生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+  // button(完了)タグの生成
+  const completeButtont = document.createElement("button");
+  completeButtont.innerText = "完了";
+  // button(削除)タグの生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  // div タグの子要素に各要素を配置
+  div.appendChild(p);
+  div.appendChild(completeButtont);
+  div.appendChild(deleteButton);
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
